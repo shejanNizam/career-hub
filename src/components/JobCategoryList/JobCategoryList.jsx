27bd417f8d7fbe38d46356b9d37b25931a54a4f1
shejanNizam <nizam.shejan11@gmail.com>
2023-04-10 14:58:27 +0600

    job category list added

--- src/components/JobCategoryList/JobCategoryList.jsx
+++ src/components/JobCategoryList/JobCategoryList.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import icon1 from "../../assets/Icons/accounts 1.png";
+import icon2 from "../../assets/Icons/business 1.png";
+import icon4 from "../../assets/Icons/chip 1.png";
+import icon3 from "../../assets/Icons/social-media 1.png";
 
 const JobCategoryList = () => {
+  const jobs = [
+    {
+      id: 1,
+      icon: icon1,
+      title: "Account & Finance",
+      available: "300 Jobs Available",
+    },
+    {
+      id: 2,
+      icon: icon2,
+      title: "Creative Design",
+      available: "100+ Jobs Available",
+    },
+    {
+      id: 3,
+      icon: icon3,
+      title: "Marketing & Sales",
+      available: "150 Jobs Available",
+    },
+    {
+      id: 4,
+      icon: icon4,
+      title: "Engineering Job",
+      available: "224 Jobs Available",
+    },
+  ];
+
   return (
     <div>
-      <h3>This is Job Category</h3>
+      <div className="text-center mt-16">
+        <h2 className="font-bold text-4xl">Job Category List</h2>
+        <p className="my-4">
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
+      </div>
+      <div className="flex justify-center items-center ">
+        {jobs.map((job) => (
+          <div key={job.id}>
+            <div className="rounded w-60 h-40 m-4 p-8 bg-slate-50">
+              <img src={job.icon} alt="#" />
+              <h4 className="font-semibold"> {job.title} </h4>
+              <p> {job.available} </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
